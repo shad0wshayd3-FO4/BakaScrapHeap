e4c641865c8a25f11c47fca00291c69071645095
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2026-08-23 22:47:47 -0600

    feat: project update

--- xmake.lua
+++ xmake.lua
@@ -30,3 +30,6 @@ target("BakaScrapHeap")
 
     -- add extra files
     add_extrafiles(".clang-format")
+
+    -- add install files
+    add_installfiles("res/*.ini", { prefixdir = "F4SE/Plugins" })
